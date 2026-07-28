01f9b877d95b7c3dc05335003bb6227a5cfc41e2
Tejas <tejas.jagdish.patil23@gmail.com>
2026-07-28 12:28:41 +0530

    Added meta data

--- Backend/DUMP/t.py
+++ Backend/DUMP/t.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 app = FastAPI(
+    title="Margadarshak API",
+    description="Backend API for the Margadarshak project",
     docs_url="/docs", 
 )
-@app.get("/hello")
+@app.get("/hello" , tags=["Trail"], summary="Returns a welcome message")
 def say_hello():
     return {"message": "Hello, welcome to FastAPI!"}
